3f8151c8cf4cc7be6cee3474314c1b4300ba2e9a
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-02-13 22:05:12 +0700

    Full code

--- ViDuNhapXuat1/src/ptns/ntu/CTTinhTong.java
+++ ViDuNhapXuat1/src/ptns/ntu/CTTinhTong.java
@@ -1,10 +1,23 @@
 package ptns.ntu;
 
+import java.util.Scanner;
+
 public class CTTinhTong {
 
 	public static void main(String[] args) {
-		
-
+		//In ra một dòng tiêu đề app
+		System.out.print("CT Tinh tong \n");
+		//Chuẩn bị cho việc nhập
+		Scanner banPhim = new Scanner(System.in);
+		//In dong huong dan moi nhap A
+		System.out.println("Moi nhap a: ");
+		double a = banPhim.nextDouble();
+		System.out.println("Moi nhap b: ");
+		double b = banPhim.nextDouble();
+		//tinh toan
+		double tong = a+b;
+		System.out.print("Tong la: ");
+		System.out.print(tong);
 	}
 
 }
