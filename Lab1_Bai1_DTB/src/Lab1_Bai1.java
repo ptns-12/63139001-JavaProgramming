9218e29d71a7402c40ba8c66950373a6fca09bd4
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-02-14 21:49:00 +0700

    Khoi tao va nhap du lieu

---
+++ Lab1_Bai1_DTB/src/Lab1_Bai1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Lab1_Bai1 {
+
+	public static void main(String[] args) {
+		// Khoi tao Scanner de nhap du lieu tu ban phim 
+		Scanner scanner = new Scanner(System.in);
+		
+		// Nhap ho va ten sinh vien
+		System.out.print("Họ và tên: ");
+		String hoten = scanner.nextLine();
+		
+		//Nhap diem trung binh 
+		System.out.print("Điểm TB: ");
+		double diemTB = scanner.nextDouble();
+		
+		
+	}
+
+}
