4a011583a28becfb279bd22abf84ea516f11491d
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-02-14 20:13:56 +0700

    khoi tao

--- ViduHuongDoiTuong/src/SanPham.java
+++ ViduHuongDoiTuong/src/SanPham.java
@@ -1,4 +1,21 @@
 
 public class SanPham {
-
+	//Định nghĩa các thuộc tính
+	int maSP;
+	String tenSP;
+	String loaiSP;
+	String anhSP; //ten file anh
+	//ham tao
+	public SanPham() {
+		//khoi tao gia tri ban dau cho cac thuoc tinh neu co o day
+		
+	}
+	public SanPham(int _maSP, String _tenSP, String _loaiSP, String _anhSP) {
+		//khoi tao gia tri ban dau cho cac thuoc tinh neu co o day
+		maSP = _maSP;
+		tenSP = _tenSP;
+		loaiSP = _loaiSP;
+		anhSP = _anhSP;
+	}
+	//cac getter, setter
 }
