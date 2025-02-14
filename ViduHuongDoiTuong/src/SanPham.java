4c0a516be4b1dc6716e1460d7175020c3315bb86
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-02-14 21:07:46 +0700

    update va chay chuong trinh

--- ViduHuongDoiTuong/src/SanPham.java
+++ ViduHuongDoiTuong/src/SanPham.java
@@ -5,17 +5,56 @@ public class SanPham {
 	String tenSP;
 	String loaiSP;
 	String anhSP; //ten file anh
-	//ham tao
+	
 	public SanPham() {
-		//khoi tao gia tri ban dau cho cac thuoc tinh neu co o day
-		
+		super();
 	}
-	public SanPham(int _maSP, String _tenSP, String _loaiSP, String _anhSP) {
-		//khoi tao gia tri ban dau cho cac thuoc tinh neu co o day
-		maSP = _maSP;
-		tenSP = _tenSP;
-		loaiSP = _loaiSP;
-		anhSP = _anhSP;
+
+	public SanPham(int maSP, String tenSP, String loaiSP, String anhSP) {
+		super();
+		this.maSP = maSP;
+		this.tenSP = tenSP;
+		this.loaiSP = loaiSP;
+		this.anhSP = anhSP;
 	}
-	//cac getter, setter
+
+	public int getMaSP() {
+		return maSP;
+	}
+
+	public void setMaSP(int maSP) {
+		this.maSP = maSP;
+	}
+
+	public String getTenSP() {
+		return tenSP;
+	}
+
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+
+	public String getAnhSP() {
+		return anhSP;
+	}
+
+	public void setAnhSP(String anhSP) {
+		this.anhSP = anhSP;
+	}
+
+	@Override
+	public String toString() {
+		return "[Mã SP=" + maSP + ", Tên SP=" + tenSP + ", Loại SP=" + loaiSP + "]";
+	}
+	
+	
+	
 }
