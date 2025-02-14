2827bdde8dd04fd77c436ca23df58dac348ae035
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-02-14 23:47:56 +0700

    Update ChuviDT.java
    full code

--- Lab1_Bai2_ChuviDT/src/ChuviDT.java
+++ Lab1_Bai2_ChuviDT/src/ChuviDT.java
@@ -12,7 +12,18 @@ public class ChuviDT {
 		System.out.print("Chiều rộng: ");
 		double rong = scanner.nextDouble();
 		
+		//tinh chu vi, dien tich va canh nho nhat
+		double chuVi = (dai + rong) * 2;
+		double dienTich = dai * rong;
+		double canhNhoNhat = Math.min(dai, rong);
 		
+		//xuat thong tin ra man hinh 
+		System.out.printf("Chu vi: %.2f\n", chuVi);
+		System.out.printf("Diện tích: %.2f\n", dienTich);
+		System.out.printf("Cạnh nhỏ nhất: %.2f\n", canhNhoNhat);
+		
+		//dong Scanner
+		scanner.close();
 
 	}
 
