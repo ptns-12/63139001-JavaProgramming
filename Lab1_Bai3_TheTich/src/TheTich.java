13c5685919c28e012d42220bf5d7e72333526c4c
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-02-15 22:17:12 +0700

    Update TheTich.java

--- Lab1_Bai3_TheTich/src/TheTich.java
+++ Lab1_Bai3_TheTich/src/TheTich.java
@@ -2,8 +2,14 @@
 public class TheTich {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Khoi tao scanner de nhap du lieu tu ban phim 
+		Scanner scanner = new Scanner(System.in);
+		
+		//nhap canh cua khoi lap phuong
+		System.out.print("Nhap canh cua khoi lap phuong: ");
+		double canh = scanner.nextDouble();
+		
+		
 	}
 
 }
